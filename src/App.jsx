446d4374408d217ece8b7378018051eeae475a7f
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-06 16:05:52 +0800

    Apply async await and add a fetching loader

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,22 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-
+  const [loading, setLoading] = useState(false);
   const fetchMoviesHandler = async () => {
-    await fetch(`https://swapi.dev/api/films`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        const transformedData = data.results.map((data) => {
-          return {
-            // ...data,
-            id: data.episode_id,
-            title: data.title,
-            openingText: data.opening_crawl,
-            releaseDate: data.release_date,
-          };
-        });
-        setMovies(transformedData);
-      })
-      .catch((e) => {
-        console.log("Error in fetching data:", e);
-      });
+    setLoading(true);
+    const response = await fetch(`https://swapi.dev/api/films`);
+    const data = await response.json();
+
+    const transformedData = data.results.map((data) => {
+      return {
+        id: data.episode_id,
+        title: data.title,
+        openingText: data.opening_crawl,
+        releaseDate: data.release_date,
+      };
+    });
+    setMovies(transformedData);
+    setLoading(false);
   };
 
   return (
@@ -33,7 +28,13 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loading && movies.length === 0 && <p>No Movies Available</p>}
+        {!loading ? (
+          <MoviesList movies={movies} />
+        ) : (
+          <p>Fetching Data Please Wait....</p>
+        )}
+        {}
       </section>
     </>
   );
