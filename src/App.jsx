c31c81c707e9a169ef7cadce14a4cdcdff511665
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-06 15:50:38 +0800

    fetch data from star wars api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = async () => {
+    await fetch(`https://swapi.dev/api/films`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const transformedData = data.results.map((data) => {
+          return {
+            // ...data,
+            id: data.episode_id,
+            title: data.title,
+            openingText: data.opening_crawl,
+            releaseDate: data.release_date,
+          };
+        });
+        setMovies(transformedData);
+      })
+      .catch((e) => {
+        console.log("Error in fetching data:", e);
+      });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
